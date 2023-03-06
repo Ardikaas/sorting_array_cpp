2b9ab5c16fbc8159f67f11f1de1b3d22ca50d538
Ardikaas <ardikaajisetiawan@gmail.com>
2023-03-06 22:59:13 +0700

    mengupdate Main.cpp bubble sort menjadi model presentasi

--- Main.cpp
+++ Main.cpp
@@ -1,74 +1,85 @@
 #include <iostream>
+
 using namespace std;
 
-float total;
-char operasi;
-int x;
-int y;
-int i;
+void bubble(){
 
-void penjumlahan(float a, float b){
-  cout << a << " + " << b << endl; 
-  total = a + b;
-}
-void pengurangan(float a, float b){
-  cout << a << " - " << b << endl; 
-  total = a - b;
-}
-void perkalian(float a, float b){
-  cout << a << " * " << b << endl; 
-  total = a * b;
-}
-void pembagian(float a, float b){
-  cout << a << " / " << b << endl; 
-  total = a / b;
 }
 
 int main(int argc, char const *argv[])
 {
-  cout << "Selamat Datang di Aplikasi Kalkulator" << endl;
-  cout << "\nPerhatian!!!" << endl;
-  cout << "Masukkan \"+\" untuk operasi penjumlahan" << endl;
-  cout << "Masukkan \"-\" untuk operasi pengurangan" << endl;
-  cout << "Masukkan \"*\" untuk operasi perkalian" << endl;
-  cout << "Masukkan \"/\" untuk operasi pembagian" << endl;
-  cout << "\nMasukkan bilangan pertama\t: ";
-  cin >> x;
-  cout << "Masukkan jenis operasi\t\t: ";
-  cin >> operasi;
-  i = 0;
-  while (i < 1){
-    if (operasi == '+' || operasi == '-'|| operasi == '*' || operasi == '/'){
-      i++;
-    }else{
-      system("cls");
-      cout << "\nOperasi yang anda masukkan salah!!!" << endl;
-      cout << "--------------------------------------" << endl;
-      cout << "\nPerhatian!!!" << endl;
-      cout << "Masukkan \"+\" untuk operasi penjumlahan" << endl;
-      cout << "Masukkan \"-\" untuk operasi pengurangan" << endl;
-      cout << "Masukkan \"*\" untuk operasi perkalian" << endl;
-      cout << "Masukkan \"/\" untuk operasi pembagian" << endl;
-      cout << "Masukkan bilangan pertama\t: " << x << endl;
-      cout << "Masukkan jenis operasi\t\t: ";
-      cin >> operasi;
-    }
+  int temp;
+  int arr[]={80, 21, 35, 42, 40, 15};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  // ***Bubble sort*** //
+  cout << "bubble sort\n================" << endl;
+  cout << "unsorted array :" << endl;
+  for (int i = 0; i<n;i++){
+    cout << arr[i] << "\t";
   }
-  cout << "Masukkan bilagan kedua\t\t: ";
-  cin >> y;
   cout << "\n";
 
-  if (operasi == '+'){
-    penjumlahan(x, y);
-  }else if (operasi == '-'){
-    pengurangan(x, y);
-  }else if (operasi == '*'){
-    perkalian(x, y);
-  }else if (operasi == '/'){
-    pembagian(x, y);
+  for (int i =0;i <n;i++){
+    cout << "i : " << i << endl;
+    cout << "arr[i] : " << arr[i] << endl;
+    cin.get();
+    for (int j=0;j<n;j++){
+      cout << "arr[i] : " << arr[i] << endl;
+      cout << "j : " << j << endl;
+      cout << "arr[j] : " << arr[j] << endl;
+      cin.get();
+      if (arr[j] < arr[i]){
+        temp = arr[i];
+        cout << "temp : " << temp << endl;
+        cin.get();
+        arr[i] = arr[j];
+        cout << "arr[i] : " << arr[i] << endl;
+        cin.get();
+        arr[j] = temp;
+        cout << "arr[i] : " << arr[j] << endl;
+        cin.get();
+      }
+      cout << "unsorted array :" << endl;
+      for (int a = 0; a<n;a++){
+        cout << arr[a] << "\t";
+      }
+      cout << "\n";
+    }
   }
-  cout << "\nTotal : " << total << endl;
-  cin.get();
-  cin.get();
+  cout << "\nsorted array :" << endl;
+  for (int i=0;i<n;i++){
+    cout << arr[i] << "\t";
+  }
+  cout << "\n\n";
+  // ***Bubble sort*** //
+
+  // ***Selection sort*** //
+  int min;
+  int p;
+  cout << "selection sort\n================" << endl;
+  cout << "unsorted array :" << endl;
+  for (int i = 0; i<n;i++){
+    cout << arr[i] << "\t";
+  }
+
+  for(int i=0;i<n-1;i++){
+    min = arr[i];
+    p = i;
+    for(int j=i+1;j<n;j++){
+      if (min > arr[j]){
+        min = arr[j];
+        p = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[p];
+    arr[p] = temp;
+  }
+  cout << "\nsorted array :" << endl;
+  for (int i=0;i<n;i++){
+    cout << arr[i] << "\t";
+  }
+  cout << "\n\n";
+  // ***Selection sort*** //
   return 0;
 }
